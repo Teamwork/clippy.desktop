436fb7069880beae1643fade3e8ed6143f94ffa5
Adam Lynch <contact@adamlynch.com>
2016-02-05 16:44:50 +0000

    Other stuff

--- script.js
+++ script.js
@@ -1,54 +1,64 @@
-var originalOn = $.fn.on;
-var win = require('nw.gui').Window.get();
+var gui = require('nw.gui');
+var win = gui.Window.get();
 var isMac = require('os').platform() === 'darwin';
 win.showDevTools(); // uncomment if you want to debug
 
 if(isMac) {
     document.title = '\u3000'; // to get around https://github.com/nwjs/nw.js/issues/3645
-    var hasInterceptedClickHandlerBinding = false;
 }
 
-$.fn.on = function(event, callback){
-    // ignore dragging of clippy within the window since the window is draggable itself
-    if(event === 'mousedown'){
-        return;
-    }
-
-    var args = arguments;
-
-    // Map the typical double-click actions to single clicks instead
-    if(event === 'dblclick'){
-        args[0] = 'click';
-
-        // To workaround https://github.com/nwjs/nw.js/issues/2387
-        // Long story -> short: first click = focus window, second click = click
-        // So now the click callback is called when the app is focused too
-        if(isMac && !hasInterceptedClickHandlerBinding) {
-            hasInterceptedClickHandlerBinding = true;
-            win.on('focus', callback);
-        }
-    }
-
-    return originalOn.apply(this, args);
-};
-
+// don't let clippy.js add any handlers
+$.fn.on = function(){};
 
 // show clippy
 clippy.load('Clippy', function(agent){
     agent.show();
-    agent.speak("Need some help closing me? Try double-clicking...");
-});
+    var intiialSpeechTimeoutId = setTimeout(function() {
+        agent.speak("Need some help closing me? Try double-clicking...");
+    }, 20000);
 
-// The 'hidden' class is being toggled to use opacity to hide flash / glitch when showing again
-//
-// Note: the .desktopClippy namespace here prevents this callback from being mapped to the focus
-// event above
-$(window).on('dblclick.desktopClippy', function(){
-    win.hide();
-    document.body.classList.add('hidden');
-
+    var windowX = null;
+    var windowY = null;
     setTimeout(function(){
-        win.show();
-        document.body.classList.remove('hidden');
-    }, 2000);
+        windowX = win.x;
+        windowY = win.y;
+    }, 250);
+
+    // to be safe: use focus as a click handler kinda, to trigger animations. Blur when done so every click triggers
+    // an animation
+    win.on('focus', function(){
+        agent.animate();
+        win.blur();
+    });
+
+    // In case the OS doesn't support blurring, we'll just call animate every now and again anyway
+    setInterval(function(){
+        agent.animate();
+    }, 12000);
+
+    // Since double-clicking draggable areas triggers maximizing on some platforms, when tell the user double-clicking
+    // closes clippy but actually we'll hide the window, unmaximize, resize back to the normal size and show again
+    // with a speech bubble
+    win.on('maximize', function(){
+        document.body.classList.add('hidden');
+        win.hide();
+        if(intiialSpeechTimeoutId) {
+            clearTimeout(intiialSpeechTimeoutId);
+            intiialSpeechTimeoutId = null;
+        }
+
+        setTimeout(function(){
+            win.unmaximize();
+            win.resizeTo(gui.App.manifest.window.width, gui.App.manifest.window.height);
+            win.moveTo(windowX, windowY);
+        }, 250);
+
+        setTimeout(function(){
+            win.show();
+            document.body.classList.remove('hidden');
+            setTimeout(function(){
+                agent.speak("Need some help?");
+            }, 500);
+        }, 2000);
+    });
 });
